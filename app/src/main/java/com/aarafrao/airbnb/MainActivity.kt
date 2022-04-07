bda0ca24f3dbc7127a7daa4be8c4a30f3d48ff2f
Rao Aaraf Younus <aarafrao22@gmail.com>
2022-04-07 22:06:48 +0500

    v1

--- app/src/main/java/com/aarafrao/airbnb/MainActivity.kt
+++ app/src/main/java/com/aarafrao/airbnb/MainActivity.kt
@@ -1,11 +1,19 @@
 package com.aarafrao.airbnb
 
-import androidx.appcompat.app.AppCompatActivity
+import android.graphics.Color
+import android.graphics.LinearGradient
+import android.graphics.Shader
+import android.graphics.Shader.TileMode
 import android.os.Bundle
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
     }
 }
